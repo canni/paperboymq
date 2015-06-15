26fa4c2491fa93b7d59e2c77e0dd140e93258000
Dariusz Górecki <darek.krk@gmail.com>
2015-06-15 22:11:02 +0100

    Add desc

--- matcher/matchers.go
+++ matcher/matchers.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package matcher providers implementations for most basic AMQ match strategies.
 package matcher
 
 import (
